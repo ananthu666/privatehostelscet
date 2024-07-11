70385fe9619c71bc8e4302eb9dd0f72ef0af5b06
Gythrie <151329547+Gythrie@users.noreply.github.com>
2024-07-11 17:59:26 +0530

    Update admin.py

--- myapp/home/admin.py
+++ myapp/home/admin.py
@@ -3,3 +3,8 @@ from home.models import Hostel,Grievance
 # Register your models here.
 admin.site.register(Hostel)
 admin.site.register(Grievance)
+
+class HostelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'owner_name', 'contact_details', 'current_vacancy', 'mens_or_ladies', 'average_rent', 'mess', 'distance', 'accommodation_type', 'curfew', 'longitude', 'latitude')
+    search_fields = ('name', 'owner_name')
+    list_filter = ('mens_or_ladies', 'accommodation_type', 'mess')
