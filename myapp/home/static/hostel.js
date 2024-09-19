e2e5cc89b66adabd0cf57416c66b36fc022407be
Gythrie <151329547+Gythrie@users.noreply.github.com>
2024-09-19 12:16:36 +0530

    Revert "Filter hostels"

--- myapp/home/static/hostel.js
+++ myapp/home/static/hostel.js
@@ -1,15 +1,14 @@
-var jsonData = myList
-
+console.log("hello_world");
+console.log(myList);
 
 const markers = [];
 const map = L.map('map');
-map.setView([8.5445, 76.9041], 17)
+map.setView([8.5445, 76.9041], 17);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
 }).addTo(map);
 var marker = L.marker([8.54311, 76.90335]).addTo(map);
-marker.bindPopup("<b>Mens Hostel , CET <b>");
-
+marker.bindPopup("<b>Mens Hostel, CET</b>");
 
 var cardContainer = document.getElementById('right');
 
@@ -17,96 +16,68 @@ function renderHostels(hostels) {
     cardContainer.innerHTML = '';
 
     hostels.forEach(data => {
-        var card = document.createElement('div');
-        card.className = 'card';
+        if (data.approval_hostel_status.trim().toLowerCase() === 'approved') {
+            var card = document.createElement('div');
+            card.className = 'card';
 
-        card.innerHTML = `
-        
-            <div class='hostel' style="display:flex; justify-content:center;align-items:center;">
-                <img style='height:2.5rem ;width:4rem;object-fit:cover;align-self:flex-start;margin-top:5px' src="/static/assets/hostel.jpg" alt="" />
-            </div>
-            <div class='details'>
-                <h3>${data.name}</h3>
-                <p>Vacancy: ${data.current_vacancy}</p>
-                <div class='additional-info' style='display:none;'>
-                <p>Address: ${data.address}</p>
-                <p>Rent: ${data.average_rent}</p>
-                <p>Owner: ${data.owner_name}</p>
-                <p>Total Capacity: ${data.total_capacity}</p>
-                <p>Contact details: ${data.contact_details}</p>
-                <p>Curfew: ${data.curfew}</p>
-                <p>Mens/Ladies: ${data.mens_or_ladies}</p>
-                <p>Mess Availability: ${data.mess ? 'Yes' : 'No'}</p>
-                <p>Distance from College: ${data.distance} km</p>
+            card.innerHTML = `
+                <div class='hostel' style="display:flex; justify-content:center; align-items:center;">
+                    <img style='height:2.5rem; width:4rem; object-fit:cover; align-self:flex-start; margin-top:5px' src="/static/assets/hostel.jpg" alt="" />
                 </div>
-                <button class='view-more-btn'>View More</button>
-            </div>
-        `;
-        cardContainer.appendChild(card);
+                <div class='details'>
+                    <h3>${data.name}</h3>
+                    <p>Vacancy: ${data.current_vacancy}</p>
+                    <div class='additional-info' style='display:none;'>
+                        <p>Address: ${data.address}</p>
+                        <p>Rent: ${data.average_rent}</p>
+                        <p>Owner: ${data.owner_name}</p>
+                        <p>Contact details: ${data.contact_details}</p>
+                        <p>Curfew: ${data.curfew}</p>
+                        <p>Mens/Ladies: ${data.mens_or_ladies}</p>
+                        <p>Mess Availability: ${data.mess ? 'Yes' : 'No'}</p>
+                        <p>Distance from College: ${data.distance} km</p>
+                    </div>
+                    <button class='view-more-btn'>View More</button>
+                </div>
+            `;
+            cardContainer.appendChild(card);
 
-        markers[data.id] = L.marker([data.latitude, data.longitude]).addTo(map);
-        markers[data.id].bindPopup(`<b>${data.name}<b>`);
+            markers[data.id] = L.marker([data.latitude, data.longitude]).addTo(map);
+            markers[data.id].bindPopup(`<b>${data.name}</b>`);
 
-        card.querySelector('.view-more-btn').addEventListener('click', (event) => {
-            // Toggle the visibility of additional information
-            const additionalInfo = card.querySelector('.additional-info');
-            additionalInfo.style.display = additionalInfo.style.display === 'none' ? 'block' : 'none';
+            card.querySelector('.view-more-btn').addEventListener('click', (event) => {
+                // Toggle the visibility of additional information
+                const additionalInfo = card.querySelector('.additional-info');
+                additionalInfo.style.display = additionalInfo.style.display === 'none' ? 'block' : 'none';
 
-            card.querySelector('.view-more-btn').innerText = additionalInfo.style.display === 'none' ? 'View More' : 'View Less';
-            // Prevent the card click event from being triggered
-            event.stopPropagation();
-        });
+                card.querySelector('.view-more-btn').innerText = additionalInfo.style.display === 'none' ? 'View More' : 'View Less';
+                // Prevent the card click event from being triggered
+                event.stopPropagation();
+            });
 
-        card.addEventListener('click', () => {
-            // Set the view of the map to the clicked card's location
-            map.setView([data.latitude, data.longitude], 17);
-            markers[data.id].openPopup();
-        });
+            card.addEventListener('click', () => {
+                // Set the view of the map to the clicked card's location
+                map.setView([data.latitude, data.longitude], 17);
+                markers[data.id].openPopup();
+            });
 
-        markers[data.id].on('click', () => {
-            // Open Google Maps with the marker's location
-            const googleMapsUrl = `https://www.google.com/maps?q=${data.latitude},${data.longitude}`;
-            window.open(googleMapsUrl, '_blank');
-        });
+            markers[data.id].on('click', () => {
+                // Open Google Maps with the marker's location
+                const googleMapsUrl = `https://www.google.com/maps?q=${data.latitude},${data.longitude}`;
+                window.open(googleMapsUrl, '_blank');
+            });
+        }
     });
 }
 
 function searchHostels() {
     var searchInput = document.getElementById("searchInput").value.toLowerCase();
-    filterHostels(searchInput);
-}
 
-
-function filterHostels(searchInput) {
-    var filterGender = document.getElementById('filterGender').value.toLowerCase();
-    var filterMinVacancy = parseInt(document.getElementById('filterMinVacancy').value);
-    var filterMessAvailability = document.getElementById('filterMessAvailability').value.toLowerCase();
-    
-    var filteredHostels = jsonData.filter(function (hostel) {
-        return (!searchInput || hostel.name.toLowerCase().startsWith(searchInput)) &&
-            (!filterGender || hostel.mens_or_ladies.toLowerCase() === filterGender) &&
-            (isNaN(filterMinVacancy) || hostel.current_vacancy >= filterMinVacancy) &&
-            (!filterMessAvailability || hostel.mess_availability.toLowerCase() === filterMessAvailability);
+    var filteredHostels = myHostelApprovals.filter(function (hostel) {
+        return hostel.name.toLowerCase().includes(searchInput) && hostel.approval_hostel_status.trim().toLowerCase() === 'approved';
     });
+
     renderHostels(filteredHostels);
 }
 
-function resetFilters() {
-    document.getElementById("searchInput").value = '';
-    document.getElementById("filterGender").value = '';
-    document.getElementById("filterMinVacancy").value = '';
-    document.getElementById("filterMessAvailability").value = '';
-    
-    filterHostels('');
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    resetFilters(); 
-});
-
-document.getElementById('searchInput').addEventListener('input', searchHostels);
-document.getElementById('filterGender').addEventListener('change', searchHostels);
-document.getElementById('filterMinVacancy').addEventListener('input', searchHostels);
-document.getElementById('filterMessAvailability').addEventListener('change', searchHostels);
-
-renderHostels(jsonData);
+renderHostels(myList);
