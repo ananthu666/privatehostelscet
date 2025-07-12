27928be61dcf8e8e5cea57fb5956d481ce129741
sreelaltom <tve22131@cet.ac.in>
2025-07-12 22:56:58 +0530

    commit before hosting on versal

--- myapp/build_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build script for Vercel
-echo "Installing dependencies..."
-pip install -r requirements.txt
-
-echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
